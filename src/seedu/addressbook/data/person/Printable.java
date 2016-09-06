396983a03ca85cdbd3db5559a010088e2e00c13a
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-09-06 21:57:25 +0800

    create Printable interface for things that are... erm... printable

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents an object with a printable string representation  
+ */
+public interface Printable {
+    public String getPrintableString();
+}
